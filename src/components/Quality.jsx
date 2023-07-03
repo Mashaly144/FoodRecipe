b46fcdc940e39e34874170f2b55abbd6fd2fe136
Mashaly144 <117321633+Mashaly144@users.noreply.github.com>
2023-07-04 00:10:59 +0300

    Update Quality.jsx

--- src/components/Quality.jsx
+++ src/components/Quality.jsx
@@ -11,12 +11,12 @@ const Quality = () => {
           <div className='justify-center'>
             <img src={hero2} className='w-[400px]' alt='hero' />
           </div>
-          <div className='flex flex-col  justify-between items-start space-y-4 lg:space-y-10 '>
+          <div className='flex flex-col text-center lg:text-left  justify-between items-center lg:items-start space-y-4 lg:space-y-10'>
             {/* text */}
             <h1 className='text-2xl lg:text-3xl font-semibold'>
               We are ready to deliver your favorite food items
             </h1>
-            <p className='text-dimWhite w-[450px] lg:w-[500px] text-xl'>
+            <p className='text-dimWhite  lg:w-[500px] text-[15px] lg:text-xl'>
               With our state-of-the-art delivery service, we prioritize
               efficiency without compromising on quality. Whether you're a busy
               professional.
